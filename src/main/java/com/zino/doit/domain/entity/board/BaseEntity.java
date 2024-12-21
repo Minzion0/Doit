3d0254c3fac0a798a3ab2d9ad1ad9a7335ee6b7a
minzion <aert12435@gmail.com>
2024-12-21 14:37:13 +0900

    Entity 설정
    BoardEntity

--- src/main/java/com/zino/doit/domain/entity/board/BaseEntity.java
+++ src/main/java/com/zino/doit/domain/entity/board/BaseEntity.java
@@ -1,2 +1,18 @@
-package com.zino.doit.domain.entity.board;public class BaseEntity {
+package com.zino.doit.domain.entity.board;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import java.time.LocalDateTime;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+  @CreatedBy
+  @Column(nullable = false,updatable = false)
+  private LocalDateTime createdAt;
+  @LastModifiedBy
+  @Column(nullable = false)
+  private LocalDateTime updatedAt;
 }
--- src/main/java/com/zino/doit/domain/entity/board/BoardEntity.java
+++ src/main/java/com/zino/doit/domain/entity/board/BoardEntity.java
@@ -1,2 +1,24 @@
-package com.zino.doit.domain.entity.board;public class BoardEntity {
+package com.zino.doit.domain.entity.board;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "Board")
+public class BoardEntity extends BaseEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String title;
+  @Column(nullable = false)
+  private String content;
+
+  private Long viewCount;
+
+  
+
 }
